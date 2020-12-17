716b4ee70c63baec7552b47b6a251a2f0a6c7a68
sjiang20 <sjiang086@gmail.com>
2020-12-17 11:30:44 -0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React,{Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 'snare', letter: 'Q', src: 'https://www.myinstants.com/media/sounds/snare.mp3' },
+  { id: 'bass 1', letter: 'W', src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3' },
+  { id: 'bongo', letter: 'E', src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav' },
+  { id: 'tom tom', letter: 'A', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
+  { id: 'bass 3', letter: 'S', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
+  { id: 'shotgun', letter: 'D', src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav' },
+  { id: 'high hat', letter: 'Z', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
+  { id: 'drum hit', letter: 'X', src: 'http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3' },
+  { id: 'laser', letter: 'C', src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
+]
+
+class DrumPad extends React.Component {
+ 
+  componentDidMount() {
+    console.log(this.audio)
+    document.addEventListener('keydown', this.handleKeydown)
+    window.focus()
+  }
+  
+ componentWillUnmount() {
+   document.removeEventListener('keydown', this.handleKeydown)
+ }
+  
+  handleKeydown = e => {
+    if(e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play()
+      this.audio.currentTime = 0
+      this.props.handleDisplay(this.props.id)
+    }
+  }
+ 
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+    this.props.handleDisplay(this.props.id)
+  }
+  
+  render() {
+    return (
+      <div 
+          className='drum-pad' 
+          id={this.props.id}
+          onClick={this.handleClick}
+      >
+        <h1>{this.props.letter}</h1>
+        <audio id={this.props.letter}
+               className='clip'
+               src={this.props.src}
+               ref={ref => this.audio = ref}
+          ></audio>
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      display: 'Click or Press a Key'
+    }
+  }
+  
+  handleDisplay = display => this.setState({ display })
+  
+  render(){
+    return(
+    <div className="drum" id='drum-machine'>
+        <div className="drum-display" id='display'>
+        <h1>{this.state.display}</h1>
+        </div>
+        <div className="drum-control" id='drum-pads'>{data.map(d => (
+          <div className="drum-pad"> 
+          <DrumPad
+            key={d.id}
+            id={d.id}
+            letter={d.letter}
+            src={d.src}
+            handleDisplay={this.handleDisplay}
+          />   
+          </div>
+         ))}</div>
+    </div>
+    )
+  }
+}
+
+export default App
